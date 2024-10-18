3eaebcb4e8476cab1a3502a724f2b54086707933
electricschmidt <tomdschmidt@gmail.com>
2024-10-18 12:47:02 +0100

    Added sleep

--- scripts/global/foundry.sh
+++ scripts/global/foundry.sh
@@ -45,6 +45,7 @@ sudo chmod 644 /etc/systemd/system/foundry.service
 sudo systemctl daemon-reload
 sudo systemctl start foundry
 sudo systemctl enable foundry
+sleep 10s
 
 
 # configure foundry aws json file
